240f40ada48ad15c03b4966a34f8cbbe6499b171
Valeriia <valeriia.chyrko@oa.edu.ua>
2025-02-19 18:50:18 +0200

    Add authentication with clark.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+﻿const AuthLayout = ({
+    children
+}:{
+    children:React.ReactNode
+}) => {
+  return (
+      <div className="h-full flex flex-col items-center justify-center min-h-screen bg-white">
+          {children}
+      </div>
+  );
+}
+
+export default AuthLayout;
